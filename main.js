31c37aab2ef4a63b6ea7ae7faad1dee862d7b238
Shantanu <98827369+standerd6@users.noreply.github.com>
2023-07-30 20:20:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup()
+{
+    canvas = createCanvas(550,550);
+    canvas.position(600,100);
+
+    video = createCapture(VIDEO);
+    video.size(550,550);
+}
+
+function draw()
+{
+    background(255,0,0);
+   // fill("blue");
+    //stroke("black");
+}    
